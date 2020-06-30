93df1dcbe16061da8848d909cb368c9647770800
Shray Sanan <47395036+Shraysanan@users.noreply.github.com>
2020-07-01 01:39:24 +0530

    style change and mongolab usage

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var commentRoutes=require("./routes/comments"),
     campgroundRoutes=require("./routes/campgrounds"),
     authRoutes=require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify:false
-});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify:false
+// });
+mongoose.connect("mongodb+srv://shray:shray08062000@yelpcamp.uhq45.mongodb.net/yelpcamp?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify:false})
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs")
 app.use(express.static(__dirname+"/public"))//css files are found inside public directory inside current directory
